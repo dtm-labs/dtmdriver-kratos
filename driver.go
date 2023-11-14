942f93a6be10e03860c964e7a3f8e2d7a6e05b80
tanzegen <1295620364@qq.com>
2023-11-14 16:38:42 +0800

    add nacos组件适配

--- driver.go
+++ driver.go
@@ -2,17 +2,23 @@ package driver
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/dtm-labs/dtmdriver"
 	consul "github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	etcd "github.com/go-kratos/kratos/contrib/registry/etcd/v2"
+	nacos "github.com/go-kratos/kratos/contrib/registry/nacos/v2"
 	"github.com/go-kratos/kratos/v2/registry"
 	_ "github.com/go-kratos/kratos/v2/transport/grpc/resolver/direct"
 	"github.com/go-kratos/kratos/v2/transport/grpc/resolver/discovery"
 	consulAPI "github.com/hashicorp/consul/api"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	nacosconstant "github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	etcdAPI "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc/resolver"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
@@ -21,6 +27,7 @@ const (
 	DefaultScheme = "discovery"
 	EtcdScheme    = "etcd"
 	ConsulScheme  = "consul"
+	NacosScheme   = "nacos"
 )
 
 type kratosDriver struct{}
@@ -60,7 +67,6 @@ func (k *kratosDriver) RegisterService(target string, endpoint string) error {
 		//add resolver so that dtm can handle discovery://
 		resolver.Register(discovery.NewBuilder(registry, discovery.WithInsecure(true)))
 		return registry.Register(context.Background(), registerInstance)
-
 	case ConsulScheme:
 		registerInstance := &registry.ServiceInstance{
 			Name:      strings.TrimPrefix(u.Path, "/"),
@@ -74,6 +80,8 @@ func (k *kratosDriver) RegisterService(target string, endpoint string) error {
 		//add resolver so that dtm can handle discovery://
 		resolver.Register(discovery.NewBuilder(registry, discovery.WithInsecure(true)))
 		return registry.Register(context.Background(), registerInstance)
+	case NacosScheme:
+		return k.nacosRegister(u, endpoint)
 	default:
 		return fmt.Errorf("unknown scheme: %s", u.Scheme)
 	}
@@ -94,6 +102,64 @@ func (k *kratosDriver) ParseServerMethod(uri string) (server string, method stri
 	}
 	index := strings.IndexByte(u.Path[1:], '/') + 1
 	return u.Scheme + "://" + u.Host + u.Path[:index], u.Path[index:], nil
+}
+
+func (k *kratosDriver) nacosRegister(url *url.URL, endpoint string) (err error) {
+	registerInstance := &registry.ServiceInstance{
+		Name:      strings.TrimPrefix(url.Path, "/"),
+		Endpoints: strings.Split(endpoint, ","),
+	}
+
+	hostSplit := strings.Split(url.Host, ":")
+	if len(hostSplit) != 2 {
+		return errors.New("nacos host format error")
+	}
+	ipAddr, port := hostSplit[0], uint64(8848)
+	port, err = strconv.ParseUint(hostSplit[1], 10, 64)
+	if err != nil {
+		return
+	}
+
+	sc := []nacosconstant.ServerConfig{
+		*nacosconstant.NewServerConfig(ipAddr, port),
+	}
+
+	namespaceId := url.Query().Get("namespaceId")
+	if namespaceId == "" {
+		namespaceId = "public"
+	}
+
+	timeoutMs := uint64(5000)
+	if url.Query().Get("timeoutMs") != "" {
+		timeoutMs, err = strconv.ParseUint(url.Query().Get("timeoutMs"), 10, 64)
+		if err != nil {
+			return
+		}
+	}
+
+	notLoadCacheAtStart := strings.ToLower(url.Query().Get("notLoadCacheAtStart")) != "false"
+
+	cc := nacosconstant.NewClientConfig(
+		nacosconstant.WithNamespaceId(namespaceId),
+		nacosconstant.WithTimeoutMs(timeoutMs),
+		nacosconstant.WithNotLoadCacheAtStart(notLoadCacheAtStart),
+	)
+
+	client, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ServerConfigs: sc,
+			ClientConfig:  cc,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	r := nacos.New(client)
+
+	resolver.Register(discovery.NewBuilder(r, discovery.WithInsecure(true)))
+
+	return r.Register(context.Background(), registerInstance)
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -6,14 +6,13 @@ require (
 	github.com/dtm-labs/dtmdriver v0.0.6
 	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20220414054820-d0b704b8f38d
 	github.com/go-kratos/kratos/contrib/registry/etcd/v2 v2.0.0-20220301040457-03ad2b663624
-	github.com/go-kratos/kratos/v2 v2.2.1
+	github.com/go-kratos/kratos/contrib/registry/nacos/v2 v2.0.0-20231113102135-421dbc7dae0f
+	github.com/go-kratos/kratos/v2 v2.7.1
 	github.com/hashicorp/consul/api v1.12.0
+	github.com/nacos-group/nacos-sdk-go v1.0.9
 	go.etcd.io/etcd/client/v3 v3.5.2
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
-	google.golang.org/genproto v0.0.0-20220228195345-15d65a4533f7 // indirect
-	google.golang.org/grpc v1.48.0
+	google.golang.org/grpc v1.56.1
 )
