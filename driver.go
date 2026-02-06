3ef3d1054301fed685d63aa0deb5bae36b8e29c3
tiandi <my.english.mail@qq.com>
2026-02-06 23:18:51 +0800

    support kratos etcd & consul tls. support url multi line

--- driver.go
+++ driver.go
@@ -2,18 +2,23 @@ package driver
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"log"
+	"net/url"
+	"strings"
+
 	"github.com/dtm-labs/dtmdriver"
 	consul "github.com/go-kratos/kratos/contrib/registry/consul/v2"
 	etcd "github.com/go-kratos/kratos/contrib/registry/etcd/v2"
 	"github.com/go-kratos/kratos/v2/registry"
 	_ "github.com/go-kratos/kratos/v2/transport/grpc/resolver/direct"
 	"github.com/go-kratos/kratos/v2/transport/grpc/resolver/discovery"
+	"github.com/google/uuid"
 	consulAPI "github.com/hashicorp/consul/api"
 	etcdAPI "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc/resolver"
-	"net/url"
-	"strings"
 )
 
 const (
@@ -37,21 +42,58 @@ func (k *kratosDriver) RegisterService(target string, endpoint string) error {
 	if target == "" {
 		return nil
 	}
+	//cut line break,support multi line
+	target = strings.ReplaceAll(target, "\n", "")
+	target = strings.ReplaceAll(target, "\r", "")
 
 	u, err := url.Parse(target)
 	if err != nil {
 		return err
 	}
+
+	tlsConf := &TlsConfig{}
+	tlsEnable := isTlsEnable(u)
+	if tlsEnable {
+		tlsConf, err = paseTargetUrlForTls(u)
+		if err != nil {
+			return err
+		}
+	}
+
 	switch u.Scheme {
 	case DefaultScheme:
 		fallthrough
 	case EtcdScheme:
+		newUUID, err := uuid.NewUUID()
+		if err != nil {
+			return err
+		}
 		registerInstance := &registry.ServiceInstance{
+			ID:        newUUID.String(),
 			Name:      strings.TrimPrefix(u.Path, "/"),
 			Endpoints: strings.Split(endpoint, ","),
 		}
+		var Certificates []tls.Certificate
+		var caPool = &x509.CertPool{}
+		if tlsEnable {
+			caPool, err = loadCaPool(tlsConf)
+			if err != nil {
+				return err
+			}
+
+			cert := &tls.Certificate{}
+			cert, err = loadCertificate(tlsConf)
+			if err != nil {
+				return err
+			}
+			Certificates = append(Certificates, *cert)
+		}
 		client, err := etcdAPI.New(etcdAPI.Config{
 			Endpoints: strings.Split(u.Host, ","),
+			TLS: &tls.Config{
+				RootCAs:      caPool,
+				Certificates: Certificates,
+			},
 		})
 		if err != nil {
 			return err
@@ -59,14 +101,27 @@ func (k *kratosDriver) RegisterService(target string, endpoint string) error {
 		registry := etcd.New(client)
 		//add resolver so that dtm can handle discovery://
 		resolver.Register(discovery.NewBuilder(registry, discovery.WithInsecure(true)))
-		return registry.Register(context.Background(), registerInstance)
+		//resolver.Register(discovery.NewBuilder(registry))
+		err = registry.Register(context.Background(), registerInstance)
+		if err != nil {
+			log.Println("register instance error: %v", err)
+			return err
+		}
+		return nil
 
 	case ConsulScheme:
 		registerInstance := &registry.ServiceInstance{
 			Name:      strings.TrimPrefix(u.Path, "/"),
 			Endpoints: strings.Split(endpoint, ","),
 		}
-		client, err := consulAPI.NewClient(&consulAPI.Config{Address: u.Host})
+		client, err := consulAPI.NewClient(&consulAPI.Config{
+			Address: u.Host,
+			TLSConfig: consulAPI.TLSConfig{
+				CAFile:   tlsConf.CaPath,
+				CertFile: tlsConf.CertPath,
+				KeyFile:  tlsConf.CertKeyPath,
+			},
+		})
 		if err != nil {
 			return err
 		}
--- go.mod
+++ go.mod
@@ -1,19 +1,50 @@
 module github.com/dtm-labs/dtmdriver-kratos
 
-go 1.15
+go 1.22
 
 require (
 	github.com/dtm-labs/dtmdriver v0.0.6
 	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20220414054820-d0b704b8f38d
-	github.com/go-kratos/kratos/contrib/registry/etcd/v2 v2.0.0-20220301040457-03ad2b663624
-	github.com/go-kratos/kratos/v2 v2.2.1
+	github.com/go-kratos/kratos/contrib/registry/etcd/v2 v2.0.0-20251217105121-fb8e43efb207
+	github.com/go-kratos/kratos/v2 v2.9.2
 	github.com/hashicorp/consul/api v1.12.0
-	go.etcd.io/etcd/client/v3 v3.5.2
+	github.com/pkg/errors v0.9.1
+	go.etcd.io/etcd/client/v3 v3.5.11
+	google.golang.org/grpc v1.61.1
+)
+
+require (
+	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/go-kratos/aegis v0.2.0 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-hclog v0.12.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/hashicorp/serf v0.9.6 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.11 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.11 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
-	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
-	google.golang.org/genproto v0.0.0-20220228195345-15d65a4533f7 // indirect
-	google.golang.org/grpc v1.48.0
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto v0.0.0-20231212172506-995d672761c0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
